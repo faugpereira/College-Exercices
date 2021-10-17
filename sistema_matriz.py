aedbcc8e911d761d33c973e2c55e04dba8a21ca9
Flavio Augusto <faugpereira@gmail.com>
2021-10-17 15:13:37 -0300

    updating

--- sistema_matriz.py
+++ sistema_matriz.py
@@ -1,7 +1,15 @@
 import numpy as np
 
 A = np.array([[4, 3, 2], [6, 4, 5], [2, 2, 1]])
-print('A = \n',A)
-B = np.array([42.50], [68.50], [23.50])
-print('B = \n', B)
+print('\nA = \n',A)
+B = np.array([[42.50], [68.50], [23.50]])
+print('\nB = \n', B)
 
+detA = np.linalg.det(A)
+print('\nDeterminante de A = \n', detA)
+
+inversaA = np.linalg.inv(A)
+print('\nA inversa de A = \n', inversaA)
+
+x = inversaA @ B
+print('\nX = \n', x)

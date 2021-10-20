b111e3866d5692b6a685a883c0406d4414f88f17
Flavio Augusto <faugpereira@gmail.com>
2021-10-20 20:26:58 -0300

    updating

--- valor_s_for.py
+++ valor_s_for.py
@@ -4,8 +4,7 @@ ns = 0
 
 for numerador in range(1, 100, 2):
     print('{}/{}'.format(numerador,denominador))
-    numerador = numerador + 2
-    denominador = denominador + 1
     s = (numerador/denominador)
+    denominador = denominador + 1
     ns = ns + s
 print(ns)

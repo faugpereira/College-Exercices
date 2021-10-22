e4c07d9d26098e4895518432426b3001e81bc4c1
Flavio Augusto <faugpereira@gmail.com>
2021-10-22 19:43:28 -0300

    updating

--- valor_h.py
+++ valor_h.py
@@ -1,9 +1,12 @@
 h = 0
+numerador = 0
+denominador = 0
 soma = 0
 sub = 0
 for i in range(1, 11, 1):
     h = i/(i**2)
+    sub = -h
     print('{}/{}'.format(i,i**2))
     soma += h
     re =
-print('\nA soma dos números acima é: ',soma, 'e a subtração é : ', sub)
+print('\nA soma dos números acima é: ',soma)
